6451640996745e37913ecd1231cdb72034d06aea
Rodrigo de Lazcano Perez-Vicente <rlazcano@umd.edu>
2023-02-16 01:11:01 -0500

    finish pointmaze readme

--- README.md
+++ README.md
@@ -12,14 +12,34 @@ pip install -r requirements.txt
 
 ## Create datasets
 ### Point Maze
-The point maze datasets have been regenerated using the same `q_iteration` expert policy as in the original D4RL paper. The environments used can be found [here]().
+The point maze datasets have been regenerated using the same `q_iteration` expert policy as in the original D4RL paper. The environments used can be found [here](https://robotics.farama.org/envs/maze/point_maze/).
 
 You can run the script used to regenerate the datasets with:
 ```
-python scripts/pointmaze/create_pointmaze_dataset --env "
+python scripts/pointmaze/create_pointmaze_dataset --env "PointMaze_UMaze-v3" --dataset_name="pointmaze-umaze-v0" --maze-solver="QIteration"
 ```
+
+This will generate a local Minari dataset named `pointmaze-umaze-v0` for the `PointMaze_UMaze-v3` environment, using `q_iteration` as the expert policy, Depth First Search can
+also be used as the algorithm to generate a path to the goal by passing "DFS" instead of "QIteration".
+
 ### Adroit Hand
 The Minari datasets for the Adroit Hand environments
+
+```
+python recreate_adroit_door.py
+```
+
+```
+python recreate_adroit_hammer.py
+```
+
+```
+python recreate_adroit_pen.py
+```
+
+```
+python recreate_adroit_relocate.py
+```
 
 ### More datasets to come
 * `AntMaze`
--- scripts/adroit/recreate_adroit_door.py
+++ scripts/adroit/recreate_adroit_door.py
@@ -2,7 +2,7 @@ import h5py
 import minari
 import gymnasium as gym
 from utils import AdroitStepPreProcessor
-from minari.utils.other_option_data_collector import DataCollectorV0
+from minari import DataCollectorV0
 from gymnasium_robotics.envs.adroit_hand.wrappers import SetInitialState
 
 
@@ -10,6 +10,8 @@ if __name__ == "__main__":
 
     env = SetInitialState(gym.make('AdroitHandDoor-v1', max_episode_steps=600))
     env = DataCollectorV0(env, step_preprocessor=AdroitStepPreProcessor, record_infos=True, max_steps_buffer=200000)
+    
+    
 
     with h5py.File('/home/rodrigo/Downloads/door-cloned-v1.hdf5', 'r') as f:
         qposes = f['infos']['qpos'][:]
@@ -18,11 +20,10 @@ if __name__ == "__main__":
         observations = f['observations'][:]
         door_poses = f['infos']['door_body_pos'][:]
         timeouts = f['timeouts'][:]
-        rewards = f['rewards'][:]
         
     reset = True
     last_episode_step = 0
-    for i, (timeout, observation, action, door_pos, reward, qpos, qvel) in enumerate(zip(timeouts, observations, actions, door_poses, rewards, qposes, qvels)):
+    for i, (timeout, observation, action, door_pos, qpos, qvel) in enumerate(zip(timeouts, observations, actions, door_poses, qposes, qvels)):
         if reset:
             state_dict = {'qpos': qpos, 'qvel': qvel, 'door_body_pos': door_pos}
             env.reset(initial_state_dict=state_dict)      
